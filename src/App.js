1879c492faf5a12005ae33e72c3244dbce668f18
LordDrakkarxD <araujogalhardo@hotmail.com>
2022-03-19 04:54:22 -0300

    we created authentication page component alongside sign in form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.jsx";
 import Navigation from "./routes/navigation/navigation.jsx";
-import SignIn from "./routes/sign-in/sign-in.jsx";
+import Authentication from "./routes/authentication/authentication.jsx";
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -14,7 +14,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
