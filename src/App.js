fa764b9aa5bc1d1972d3632b179b527888510722
LordDrakkarxD <araujogalhardo@hotmail.com>
2022-04-07 11:35:10 -0300

    performance optimizations

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import { selectCurrentUser } from "./store/user/user.selector";
 
-import Home from "./routes/home/home";
-import Navigation from "./routes/navigation/navigation";
-import Authentication from "./routes/authentication/authentication";
-import Shop from "./routes/shop/shop";
-import Checkout from "./routes/checkout/checkout";
+import Spinner from "./components/spinner/spinner";
+
 import { checkUserSession } from "./store/user/user.action";
+
+const Authentication = lazy(() =>
+  import("./routes/authentication/authentication")
+);
+const Shop = lazy(() => import("./routes/shop/shop"));
+const Checkout = lazy(() => import("./routes/checkout/checkout"));
+const Home = lazy(() => import("./routes/home/home"));
+const Navigation = lazy(() => import("./routes/navigation/navigation"));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -26,14 +31,16 @@ const App = () => {
   );
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={redirectHandler} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={redirectHandler} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
